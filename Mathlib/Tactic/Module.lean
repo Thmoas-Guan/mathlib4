bb540b55468392f797794ecc75bc99032229c98a
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-08-24 21:57:23 -0400

    wip

--- Mathlib/Tactic/Module.lean
+++ Mathlib/Tactic/Module.lean
@@ -2,6 +2,7 @@ import Qq
 import Mathlib.Algebra.Group.Defs
 import Mathlib.Algebra.Group.Action.Defs
 import Mathlib.Algebra.BigOperators.Group.List
+import Mathlib.GroupTheory.GroupAction.BigOperators
 
 open Lean Qq
 
@@ -12,21 +13,29 @@ abbrev smulRightFst {R S M : Type*} [SMul S R] [SMul S M] (l : List (R × M)) (s
     List (R × M) :=
   List.map (fun (⟨r, x⟩ : R × M) ↦ (s • r, x)) l
 
--- more typeclass assumptions needed
-theorem bar {R S M : Type*} [Add M] [Zero M] [SMul S R] [SMul S M] [SMul R M] [IsScalarTower S R M]
-    {x : M} {l : List (R × M)} (h : x = smulAndSum l) (s : S) :
+theorem smulRightFst_pf {R S M : Type*} [AddMonoid M] [SMul S R] [DistribSMul S M] [SMul R M]
+    [IsScalarTower S R M] {x : M} {l : List (R × M)} (h : x = smulAndSum l) (s : S) :
     s • x = smulAndSum (smulRightFst l s) := by
-  sorry
+  simp only [h, smulAndSum, List.smul_sum, List.map_map]
+  congr
+  ext ⟨r, x⟩
+  simp
 
 abbrev smulLeftFst {R S M : Type*} [SMul R S] [SMul S M] (l : List (R × M)) (s : S) :
     List (S × M) :=
   List.map (fun (⟨r, x⟩ : R × M) ↦ (r • s, x)) l
 
--- more typeclass assumptions needed
-theorem foo {R S M : Type*} [Add M] [Zero M] [SMul R S] [SMul S M] [SMul R M] [IsScalarTower R S M]
+theorem smulLeftFst_pf {R S M : Type*} [AddMonoid M] [SMul R S] [DistribSMul S M] [SMul R M]
+    [SMulCommClass S R M] [IsScalarTower R S M]
     {x : M} {l : List (R × M)} (h : x = smulAndSum l) (s : S) :
     s • x = smulAndSum (smulLeftFst l s) := by
-  sorry
+  simp only [h, smulAndSum, List.smul_sum, List.map_map]
+  congr
+  ext ⟨r, x⟩
+  simp [smul_comm]
+
+theorem one_pf {M : Type*} [AddMonoid M] (x : M) : x = smulAndSum [((1:ℕ), x)] := by
+  simp [smulAndSum]
 
 partial def parse {v : Level} (M : Q(Type v)) (i : Q(AddMonoid $M)) (x : Q($M)) :
     MetaM (Σ R : Q(Type), Σ _ : Q(SMul $R $M), Σ e : Q(List ($R × $M)), Q($x = smulAndSum $e)) := do
@@ -45,16 +54,21 @@ partial def parse {v : Level} (M : Q(Type v)) (i : Q(AddMonoid $M)) (x : Q($M)) 
   | ~q(@HSMul.hSMul _ _ _ (@instHSMul $S _ $iS) $s $y) =>
     let ⟨R, iR, l, pf⟩ ← parse M i y
     try
+      let _i₂ ← synthInstanceQ q(DistribSMul $S $M)
+      assumeInstancesCommute
       let _i₃ ← synthInstanceQ q(SMul $S $R)
       let _i₄ ← synthInstanceQ q(IsScalarTower $S $R $M)
       let l' : Q(List ($R × $M)) := q(smulRightFst $l $s)
-      let pf' : Q($s • $y = smulAndSum $l') := q(bar $pf $s)
+      let pf' : Q($s • $y = smulAndSum $l') := q(smulRightFst_pf $pf $s)
       pure ⟨R, iR, l', pf'⟩
     catch _ =>
+      let _i₂ ← synthInstanceQ q(DistribSMul $S $M)
+      assumeInstancesCommute
       let _i₃ ← synthInstanceQ q(SMul $R $S)
       let _i₄ ← synthInstanceQ q(IsScalarTower $R $S $M)
-      let _i₅ ← synthInstanceQ q(SMulCommClass $R $S $M)
+      let _i₅ ← synthInstanceQ q(SMulCommClass $S $R $M)
       let l' : Q(List ($S × $M)) := q(smulLeftFst $l $s)
-      let pf' : Q($s • $y = smulAndSum $l') := q(foo $pf $s)
+      let pf' : Q($s • $y = smulAndSum $l') := q(smulLeftFst_pf $pf $s)
       pure ⟨S, iS, l', pf'⟩
-  | e => pure ⟨q(Nat), q(AddMonoid.toNatSMul), q([(1, $e)]), q(sorry)⟩
+  | _ =>
+    pure ⟨q(Nat), q(AddMonoid.toNatSMul), q([(1, $x)]), q(one_pf $x)⟩
