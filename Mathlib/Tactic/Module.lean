92f767258fd3432b26465f6851dd10a92d4ef962
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-08-24 20:46:31 -0400

    wip

--- Mathlib/Tactic/Module.lean
+++ Mathlib/Tactic/Module.lean
@@ -5,40 +5,34 @@ import Mathlib.Algebra.BigOperators.Group.List
 
 open Lean Qq
 
-abbrev ModuleExpr {u v : Level} (R : Q(Type u)) (M : Q(Type v)) : Type := Q(List ($R × $M))
+abbrev smulAndSum {R : Type*} {M : Type*} [SMul R M] [Add M] [Zero M] (l : List (R × M)) : M :=
+  (l.map (fun (⟨r, x⟩ : R × M) ↦ r • x)).sum
 
-abbrev ModuleExpr.sum {u v : Level} {R : Q(Type u)} {M : Q(Type v)} {_ : Q(SMul $R $M)}
-    {_ : Q(Add $M)} {_ : Q(Zero $M)} (e : ModuleExpr R M) : Q($M) :=
-  q(List.sum (List.map (fun (⟨r, x⟩ : $R × $M) ↦ r • x) $e))
-
-partial def parse {v : Level} (M : Q(Type v)) (i : Q(Add $M)) (x : Q($M)) :
-    MetaM (Σ R : Q(Type), ModuleExpr R M) := do
+partial def parse {v : Level} (M : Q(Type v)) (i : Q(AddMonoid $M)) (x : Q($M)) :
+    MetaM (Σ R : Q(Type), Σ _ : Q(SMul $R $M), Σ e : Q(List ($R × $M)), Q(smulAndSum $e = $x)) := do
   match x with
   | ~q($x₁ + $x₂) =>
-    let ⟨R₁, l₁⟩ ← parse M i x₁
-    let ⟨R₂, l₂⟩ ← parse M i x₂
+    let ⟨R₁, i₁, l₁, pf₁⟩ ← parse M i x₁
+    let ⟨R₂, i₂, l₂, pf₂⟩ ← parse M i x₂
     try
       let _i₁ ← synthInstanceQ q(Coe $R₁ $R₂)
       let l₁' : Q(List ($R₂ × $M)) := q(List.map (fun (⟨r, x⟩ : $R₁ × $M) ↦ ((r : $R₂), x)) $l₁)
-      pure ⟨R₂, q($l₁' ++ $l₂)⟩
+      pure ⟨R₂, i₂, q($l₁' ++ $l₂), q(sorry)⟩
     catch _ =>
       let _i₁ ← synthInstanceQ q(Coe $R₂ $R₁)
       let l₂' : Q(List ($R₁ × $M)) := q(List.map (fun (⟨r, x⟩ : $R₂ × $M) ↦ ((r : $R₁), x)) $l₂)
-      pure ⟨R₁, q($l₁ ++ $l₂')⟩
-  | ~q(@HSMul.hSMul $S _ _ (_) $s $x) =>
-    have x : Q($M) := x
-    let ⟨R, l⟩ ← parse M i x
-    let _i₁ ← synthInstanceQ q(SMul $R $M)
-    let _i₂ ← synthInstanceQ q(SMul $S $M)
+      pure ⟨R₁, i₁, q($l₁ ++ $l₂'), q(sorry)⟩
+  | ~q(@HSMul.hSMul _ _ _ (@instHSMul $S _ $iS) $s $x) =>
+    let ⟨R, iR, l, pf⟩ ← parse M i x
     try
       let _i₃ ← synthInstanceQ q(SMul $S $R)
       let _i₄ ← synthInstanceQ q(IsScalarTower $S $R $M)
       let l' : Q(List ($R × $M)) := q(List.map (fun (⟨r, x⟩ : $R × $M) ↦ ($s • r, x)) $l)
-      pure ⟨R, l'⟩
+      pure ⟨R, iR, l', q(sorry)⟩
     catch _ =>
       let _i₃ ← synthInstanceQ q(SMul $R $S)
       let _i₄ ← synthInstanceQ q(IsScalarTower $R $S $M)
       let _i₅ ← synthInstanceQ q(SMulCommClass $R $S $M)
       let l' : Q(List ($S × $M)) := q(List.map (fun (⟨r, x⟩ : $R × $M) ↦ (r • $s, x)) $l)
-      pure ⟨S, l'⟩
-  | e => pure ⟨q(Nat), q([(1, $e)])⟩
+      pure ⟨S, iS, l', q(sorry)⟩
+  | e => pure ⟨q(Nat), q(AddMonoid.toNatSMul), q([(1, $e)]), q(sorry)⟩
